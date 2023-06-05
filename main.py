44670eaa585cf772adcd48c6f45baa79a2ab0d56
oleinikov1991pavel <134002094+oleinikov1991pavel@users.noreply.github.com>
2023-06-05 14:56:10 +0300

    22233

---
+++ main.py
@@ -0,0 +1,30 @@
+# 9.90
+
+# someString = input('Введите предложение\n')
+#
+# someString1 = someString.replace('е', 'и')
+# print(someString1)
+
+#9,38(а)
+
+# string = input('Введите слово\n')
+# a = len(string)
+# result = string[7:12] + string[4:7] + string[0:4]
+#
+# if a == 12:
+#     print(result)
+# else:
+#     print('error')
+
+#9,105
+
+# string = input('Введите слово\n')
+#
+# if len(string) == 12:
+#     print(string)
+#     print(string[0:2] + string[-3:-11:-1] + string[10:13])
+# else:
+#     print('error')
+
+#9.153
+
